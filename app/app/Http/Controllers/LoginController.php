60752b286d43914ae9f83bb812842bf71ff4d273
TarSan7 <kvbrd110820162@gmail.com>
2021-07-26 12:45:29 +0300

    Authentification1.1

--- app/app/Http/Controllers/LoginController.php
+++ app/app/Http/Controllers/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function login(Request $request){
+    public function login(Request $request) {
         if(Auth::check()) {
             return redirect(route('user.private'));
         }
--- app/resources/views/private.blade.php
+++ app/resources/views/private.blade.php
@@ -1,13 +1,17 @@
 @extends('layout.main')
 @extends('layout.header1')
 
-@section('title') Private @endsection
+@section('title') Main Page @endsection
 
 @section('style')
     <link rel="stylesheet" href="./css/main.css">
 @endsection
 
-
 @section('content')
-    <h1 class="index-title">yeeeeeeee</h1>
+    <h1 class="index-title">Your Cards</h1>
+    <div class="container">
+        <div class="cards">
+            @if(Car)
+        </div>
+    </div>
 @endsection
